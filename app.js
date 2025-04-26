bbe5efdb3cee69bb1cf0a21fb3b58e85f8a080af
javeriatanveerdeveloper <alihussainuf456@gmail.com>
2025-04-26 11:15:39 +0000

    Add files via upload

---
+++ app.js
@@ -0,0 +1,48 @@
+let number1=prompt('Enter your first number');
+num1=parseInt(number1);
+console.log('1st number=' +num1);
+let number2=prompt('Enter your second number');
+num2=parseInt(number2);
+console.log('2nd number=' +num2);
+let sum= num1 + num2;
+console.log('sum is=' +sum);
+alert('sum is:' +sum );
+let number3=prompt('Enter your first number');
+num3=parseInt(number3);
+console.log('1st number=' +num3);
+let number4=prompt('Enter your second number');
+num4=parseInt(number4);
+console.log('2nd number=' +num4);
+let p= num3 * num4;
+console.log('product is= ' +p);
+alert('product is:' +p );
+
+let number5=prompt('Enter your first number')
+num5=parseInt(number5);
+console.log('1st number=' +num5);
+let number6=prompt('Enter your second number');
+num6=parseInt(number6);
+console.log('2nd number=' +num6);
+let s= num5 - num6;
+console.log('difference is= ' +s);
+alert('difference is:' +s );
+
+let number7=prompt('Enter your first number');
+num7=parseInt(number7);
+console.log('1st number=' +num7);
+let number8=prompt('Enter your second number');
+num8=parseInt(number8);
+console.log('2nd number=' +num8);
+let d= num7 / num8;
+console.log('dvision is= ' +d);
+alert('division is:' +d );
+
+let number9=prompt('Enter your first number');
+num9=parseInt(number9);
+console.log('1st number=' +num9);
+let number10=prompt('Enter your second number');
+num10=parseInt(number10);
+console.log('2nd number=' +num10);
+let m= num9 % num10;
+console.log('modulus is= ' +m);
+alert('modulus is:' +m );
